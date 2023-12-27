84328238ff314dda6290662b9dd584c223643049
mike <rozheluikmisha@gmail.com>
2023-12-27 13:12:40 +0200

    stylization of the form

--- src/components/JornalList/JornalList.jsx
+++ src/components/JornalList/JornalList.jsx
@@ -1,7 +1,29 @@
+import CardButton from "../CartButton/CardButton";
+import JornalItem from "../JornalItem/JornalItem";
 import "./JornalList.scss";
 
-function JornalList({ children }) {
-  return <div className="jornal-list">{children}</div>;
+function JornalList({ items }) {
+  if (items.length === 0) {
+    return <p>Записів поки нема, добав першу</p>;
+  }
+
+  const sortItems = (a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  };
+
+  return (
+    <>
+      {items.sort(sortItems).map((el) => (
+        <CardButton key={el.id}>
+          <JornalItem title={el.title} text={el.text} date={el.date} />
+        </CardButton>
+      ))}
+    </>
+  );
 }
 
 export default JornalList;
